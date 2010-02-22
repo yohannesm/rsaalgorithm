fc221ed8dce769c5fd49d0a1249de73f51cff125
DavidKraftIshihama <DavidKraftIshihama@bcb97a9e-1f57-11df-a325-4db0301e4011>
2010-02-22 02:19:07 +0000

    Skeleton code

---
+++ KeyGen.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class KeyGen{
+public static void main(String[] args) {
+	}
+}// end class KeyGen
+
+
+
+
+
+
+
---
+++ KeyGenTest.java
@@ -0,0 +1,10 @@
+import org.junit.* ;
+import static org.junit.Assert.*;
+
+public class KeyGenTest {
+
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
+}
