7655aa5799336291188745cf74e11ee6d93582c2
DavidKraftIshihama <DavidKraftIshihama@bcb97a9e-1f57-11df-a325-4db0301e4011>
2010-03-05 02:29:42 +0000

    last commit

--- trunk/RSATest.java
+++ trunk/RSATest.java
@@ -4,6 +4,12 @@ import static org.junit.Assert.*;
 public class RSATest {
     //2/26 4:00pm - 5:00 pm
     // David driving
+
+    // For this part, we mainly tested our algorithm to do modular arithmetic
+    // This is because we wanted to be certain that we were getting correct
+    // values out of that function, as it is the function that directly
+    // encrypts/decrypts our numbers.
+
     @Test
     public void test1() {
         long result = RSA.moduArith(3, 8, 5);
